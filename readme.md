d88a7c1eb941b75235567f4b0ccfde4c5138dc1f
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2021-01-07 13:57:11 -0400

    Npm Setup

---
+++ readme.md
@@ -0,0 +1,3 @@
+#Installation
+
+npm insall npm dadjoke
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+const joke = async () => {
+    const jokedata = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    const jokeObj = await jokedata.json();
+    if(!jokeObj.status === '200') {
+        return new Error(`Timeout out: Status: ${jokeObj.status}`)
+    }
+    const actualjoke = jokeObj.joke 
+    return actualjoke 
+}
+module.exports.dadjoke = joke
